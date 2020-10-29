422534369a56a6c1115282a6f00ec7ce49532fdd
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2020-10-29 10:07:04 +0300

    Update nobetciEczane.py

--- Python/02_Kaziyici/_Spatula/w_bs4/nobetciEczane.py
+++ Python/02_Kaziyici/_Spatula/w_bs4/nobetciEczane.py
@@ -4,6 +4,98 @@ import requests, json
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
+class NobetciEczane(object):
+    """
+    NobetciEczane : eczaneler.gen.tr adresinden nöbetçi eczane verilerini hazır formatlarda elinize verir.
+
+    Öz Nitelikler
+    -------------
+        il (str): Nöbetçi Eczane Bilgisi istenilen il
+        ilce (str): Nöbetçi Eczane Bilgisi istenilen ilçe
+
+    Methodlar
+    ----------
+        .veri()         -> list:
+            json verisi döndürür.
+
+        .gorsel()       -> str:
+            oluşan json verisini insanın okuyabileceği formatta döndürür.
+
+        .tablo()        -> str:
+            tabulate verisi döndürür.
+
+        .anahtarlar()   -> list:
+            kullanılan anahtar listesini döndürür.
+    """
+    def __init__(self, il:str, ilce:str):
+        super().__init__()
+
+        il      = il.replace('İ', "i").lower()
+        ilce    = ilce.lower()
+
+        tr2eng  = str.maketrans(" .,-*/+-ıİüÜöÖçÇşŞğĞ", "________iIuUoOcCsSgG")
+        il      = il.translate(tr2eng)
+        ilce    = ilce.translate(tr2eng)
+
+        url     = f"https://www.eczaneler.gen.tr/nobetci-{il}-{ilce}"
+        kimlik  = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+        istek   = requests.get(url, headers=kimlik)
+
+        corba = BeautifulSoup(istek.content, "lxml")
+        bugun = corba.find('div', id='nav-bugun')
+
+        liste = []
+        try:
+            for bak in bugun.findAll('tr')[1:]:
+                ad    = bak.find('span', class_='isim').text
+                mah   = None if bak.find('div', class_='my-2') == None else bak.find('div', class_='my-2').text
+                adres = bak.find('span', class_='text-capitalize').text
+                tarif = None if bak.find('span', class_='text-secondary font-italic') == None else bak.find('span', class_='text-secondary font-italic').text
+                telf  = bak.find('div', class_='col-lg-3 py-lg-2').text
+
+                liste.append({
+                    'ad'        : ad,
+                    'mahalle'   : mah,
+                    'adres'     : adres,
+                    'tarif'     : tarif,
+                    'telefon'   : telf
+                })
+        except AttributeError:
+            pass
+
+        self.liste  = liste
+
+    def veri(self):
+        "json verisi döndürür."
+        return self.liste or None
+
+    def gorsel(self):
+        "oluşan json verisini insanın okuyabileceği formatta döndürür."
+        return json.dumps(self.liste, indent=2, sort_keys=False, ensure_ascii=False) if self.liste else None
+
+    def tablo(self):
+        "tabulate verisi döndürür."
+        return tabulate(self.liste, headers='keys', tablefmt='psql') or None
+
+    def anahtarlar(self):
+        "kullanılan anahtar listesini döndürür."
+        return [anahtar for anahtar in self.liste[0].keys()] if self.liste else None
+
+#bakalim = NobetciEczane("İstanbul", "Beylikdüzü")
+
+#print(bakalim.veri())
+#print(bakalim.gorsel())
+#print(bakalim.tablo())
+#print(bakalim.anahtarlar())
+
+#----------------------------------------------------------------------------------------------------#
+
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+import requests, json
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
 kullanim =  {
     'kullanim' : [
         'nobetciEczane("canakkale", "merkez", "json_veri")',
